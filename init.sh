6f52cf2d3f790f781393b74a11226d6087778395
Cliff Hammerschmidt <cliff.hammerschmidt@wealthbar.com>
2021-03-30 13:10:17 -0700

    update lock files

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 IFS=$'\n'
 
-for bf in $(find . -type f -name build | grep -v node_modules)
+for bf in $(find . -type f -name init | grep -v node_modules)
 do
   pushd $(dirname "$bf") || exit
-  ./build
+  ./init
   popd || exit
 done
