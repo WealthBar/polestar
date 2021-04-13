c50a917334f4d5f8f0cbd8e5c5177338dd0f196e
Cliff Hammerschmidt <cliff.hammerschmidt@wealthbar.com>
2021-04-13 10:05:38 -0700

    fix db connection caching

--- lib/node_core/src/db/db_provider.ts
+++ lib/node_core/src/db/db_provider.ts
@@ -14,53 +14,56 @@ export type dbProviderType = <T>(
   trackingTag?: string,
 ) => Promise<T | undefined>;
 
+const dbs: Record<string, dbType> = {};
 export function dbProviderCtor(connectionString: string): dbProviderType {
-  let db: dbType;
-
   async function dbProvider<T>(
     auditUser: string,
     callback: (db: dbType) => Promise<T>,
     trackingTag = '',
   ): Promise<T | undefined> {
-    if (!db) {
-      const pgPromiseOptions = {
-        query:
-          (e) => {
+    const pgPromiseOptions = {
+      query:
+        (e) => {
+          debug(
+            'QUERY: ',
+            e.query,
+          );
+          if (e.params) {
             debug(
-              'QUERY: ',
-              e.query,
+              'PARAMS:',
+              e.params,
             );
-            if (e.params) {
-              debug(
-                'PARAMS:',
-                e.params,
-              );
-            }
-          },
-      };
-      const m = connectionString.match(/^postgres:\/\/((?<user>[^:]+):(?<password>[^@]+)@)?(?<host>[^:]+):(?<port>[^/]+)\/(?<database>[^?]+)/);
-      if (!m || !m.groups?.user || !m.groups?.host || !m.groups?.database || !m.groups?.password) {
-        throw new Error('Invalid connection string: ' + connectionString);
-      }
-      const user = m.groups.user;
-      const host = m.groups.host;
-      const port = +(m.groups?.port || '5432');
-      const database = m.groups.database;
-
-      debug('DB: %s@%s:%s/%s', user, host, port, database);
-
-      const password = m.groups.password;
-      const connectionParameters: IConnectionParameters = {
-        application_name: 'ems',
-        database,
-        host,
-        port,
-        user,
-        password,
-        ssl: host === 'localhost' ? false : {rejectUnauthorized: false},
-      };
-      db = pgPromise<Ext>(pgPromiseOptions)(connectionParameters);
+          }
+        },
+    };
+    const m = connectionString.match(/^postgres:\/\/((?<user>[^:]+):(?<password>[^@]+)@)?(?<host>[^:]+):(?<port>[^/]+)\/(?<database>[^?]+)/);
+    if (!m || !m.groups?.user || !m.groups?.host || !m.groups?.database || !m.groups?.password) {
+      throw new Error('Invalid connection string: ' + connectionString);
     }
+    const user = m.groups.user;
+    const host = m.groups.host;
+    const port = +(m.groups?.port || '5432');
+    const database = m.groups.database;
+
+    debug('DB: %s@%s:%s/%s', user, host, port, database);
+
+    const password = m.groups.password;
+    const connectionParameters: IConnectionParameters = {
+      application_name: 'ems',
+      database,
+      host,
+      port,
+      user,
+      password,
+      ssl: host === 'localhost' ? false : {rejectUnauthorized: false},
+    };
+    const key = `user ${user}, host ${host}:${port} db ${database}`;
+    if (!dbs[key]) {
+      debug('db connect:', key);
+      dbs[key] = pgPromise<Ext>(pgPromiseOptions)(connectionParameters);
+    }
+
+    const db = dbs[key];
 
     return db.tx(async (db) => {
       await db.none(
