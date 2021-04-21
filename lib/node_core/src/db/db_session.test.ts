8d1484a8c33b58a17d8cae46b178da9f4cd41a65
Travis Rempel <travis.rempel@wealthbar.com>
2021-04-21 09:50:06 -0700

    Test db_session:sessionVerify

--- lib/node_core/src/db/db_session.test.ts
+++ lib/node_core/src/db/db_session.test.ts
@@ -1,9 +1,10 @@
 import * as assert from 'assert';
-import {sessionCreate} from './db_session';
+import {serverSettingsType, userInfoType} from './../server.type';
+import {sessionCreate, sessionVerify} from './db_session';
 import {resolvedUndefined} from './../../../ts_agnostic/src/resolved';
 
 describe('sessionCreate', () => {
-  const setup = (sessionMock: object) => {
+  const setup = (dbResultMock: object) => {
     const dbProviderStub = (auditUser, callback, trackingTag) => {
       dbProviderStub.callCount++;
       callback(dbStub).catch(error => dbProviderStub.error = error);
@@ -14,7 +15,7 @@ describe('sessionCreate', () => {
 
     const dbOneStub = query => {
       dbOneStub.callCount++;
-      return sessionMock;
+      return dbResultMock;
     };
     dbOneStub.callCount = 0;
 
@@ -51,5 +52,133 @@ describe('sessionCreate', () => {
     assert.strictEqual(ctxStub.sessionId, '');
     assert.deepStrictEqual(ctxStub.session, {'': ''});
     assert.strictEqual(dbProviderStub.error.message, 'could not create a new session.');
+  });
+});
+
+describe('sessionVerify', () => {
+  const setup = (args: {
+    dbResultMock: object | null,
+    sessionIdMock: string,
+    userMock: object | null,
+    modeMock: 'client' | 'staff',
+  }) => {
+    const {dbResultMock, sessionIdMock, userMock, modeMock} = args
+
+    const dbProviderStub = (auditUser, callback, trackingTag) => {
+      dbProviderStub.callCount++;
+      callback(dbStub);
+      return resolvedUndefined;
+    };
+    dbProviderStub.callCount = 0;
+
+    const dbOneOrNoneStub = query => {
+      dbOneOrNoneStub.callCount++;
+      return dbResultMock;
+    };
+    dbOneOrNoneStub.callCount = 0;
+
+    const dbStub = () => resolvedUndefined;
+    dbStub.oneOrNone = dbOneOrNoneStub;
+
+    const ctxStub = {
+      sessionId: sessionIdMock,
+      session: {'': ''},
+      dbProvider: dbProviderStub,
+      user: null || <userInfoType>userMock,
+      db: dbStub,
+      settings: <serverSettingsType>{
+        host: 'settings_host_mock',
+        port: 'settings_port_mock',
+        schema: 'settings_schema_mock',
+        mode: modeMock,
+        appUrl: 'settings_appUrl_mock',
+        dbConnectionString: 'settings_dbConnectionString_mock',
+      }
+    }
+
+    return {ctxStub, dbProviderStub, dbStub};
+  };
+
+  it('happy path - client', async () => {
+    const {ctxStub, dbProviderStub, dbStub} = setup({
+      dbResultMock: {
+        login: 'db_result_login_mock',
+        client_profile_id: 'db_result_client_profile_id_mock',
+        federated_login_id: 'db_result_federated_login_id_mock',
+        data: 'db_result_data_mock',
+      },
+      sessionIdMock: 'session_id_mock',
+      userMock: {login: 'user_login_mock'},
+      modeMock: 'client',
+    });
+
+    await sessionVerify(ctxStub);
+    assert.strictEqual(dbProviderStub.callCount, 1);
+    assert.strictEqual(dbStub.oneOrNone.callCount, 1);
+    assert.strictEqual(ctxStub.user.login, 'db_result_login_mock');
+    assert.strictEqual(ctxStub.user.clientProfileId, undefined);
+    assert.strictEqual(ctxStub.user.federatedLoginId, undefined);
+    assert.strictEqual(ctxStub.session, 'db_result_data_mock');
+  });
+
+  it('happy path - staff', async () => {
+    const {ctxStub, dbProviderStub, dbStub} = setup({
+      dbResultMock: {
+        login: 'db_result_login_mock',
+        client_profile_id: 'db_result_client_profile_id_mock',
+        federated_login_id: 'db_result_federated_login_id_mock',
+        data: 'db_result_data_mock',
+      },
+      sessionIdMock: 'session_id_mock',
+      userMock: {login: 'user_login_mock'},
+      modeMock: 'staff',
+    });
+
+    await sessionVerify(ctxStub);
+    assert.strictEqual(dbProviderStub.callCount, 1);
+    assert.strictEqual(dbStub.oneOrNone.callCount, 1);
+    assert.strictEqual(ctxStub.user.login, 'db_result_login_mock');
+    assert.strictEqual(ctxStub.user.clientProfileId, undefined);
+    assert.strictEqual(ctxStub.user.federatedLoginId, undefined);
+    assert.strictEqual(ctxStub.session, 'db_result_data_mock');
+  });
+
+  it('Session and user do not exist', async () => {
+    const {ctxStub, dbProviderStub, dbStub} = setup({
+      dbResultMock: {
+        login: 'db_result_login_mock',
+        client_profile_id: 'db_result_client_profile_id_mock',
+        federated_login_id: 'db_result_federated_login_id_mock',
+        data: 'db_result_data_mock',
+      },
+      sessionIdMock: '',
+      userMock: null,
+      modeMock: 'client',
+    });
+
+    await sessionVerify(ctxStub);
+    assert.strictEqual(dbProviderStub.callCount, 2); // because sessionCreate called it too
+    assert.strictEqual(dbStub.oneOrNone.callCount, 1);
+    assert.strictEqual(ctxStub.user.login, 'db_result_login_mock');
+    assert.strictEqual(ctxStub.user.clientProfileId, 'db_result_client_profile_id_mock');
+    assert.strictEqual(ctxStub.user.federatedLoginId, 'db_result_federated_login_id_mock');
+    assert.strictEqual(ctxStub.session, 'db_result_data_mock');
+  });
+
+  it('No result from DB', async () => {
+    const {ctxStub, dbProviderStub, dbStub} = setup({
+      dbResultMock: null,
+      sessionIdMock: 'session_id_mock',
+      userMock: {login: 'user_login_mock'},
+      modeMock: 'client',
+    });
+
+    await sessionVerify(ctxStub);
+    assert.strictEqual(dbProviderStub.callCount, 2); // // because sessionCreate called it too
+    assert.strictEqual(dbStub.oneOrNone.callCount, 1);
+    assert.strictEqual(ctxStub.user.login, 'user_login_mock');
+    assert.strictEqual(ctxStub.user.clientProfileId, undefined);
+    assert.strictEqual(ctxStub.user.federatedLoginId, undefined);
+    assert.deepStrictEqual(ctxStub.session, {'': ''});
   });
 });
