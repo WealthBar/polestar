12d34caddddf92a22815ac8c3fbbc5ac7757fe86
Cliff Hammerschmidt <cliff.hammerschmidt@wealthbar.com>
2021-05-05 14:25:35 -0700

    skel project for .vue file builds.

---
+++ lib/vue_skel/vue_workflow.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const Vue_workflow: PluginFunction<any>;
+export default Vue_workflow;
+
+export const Vue_workflowSample: VueConstructor<Vue>;
